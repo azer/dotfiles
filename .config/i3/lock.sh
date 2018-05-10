943a6fe6c13ae25061fbaf5d5b1b8578620106d1
E.Azer Koçulu <azer@kodfabrik.com>
2018-05-10 19:36:02 +0800

    Cache lock png by size

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [ ! -f /tmp/wallpaperx.png ];
+resolution=$(xdpyinfo | awk '/dimensions/{print $2}')
+lockpng=~/lock-$resolution.png
+
+if [ ! -f $lockpng ];
 then
-    resolution=$(xdpyinfo | awk '/dimensions/{print $2}')
     convert ~/wallpaper.jpg \
             -scale "$resolution^" \
             -fill black -colorize 50% \
             -font System-San-Francisco-Display \
             -fill "#ffffff33" \
             -gravity center -pointsize 30 -annotate +0-200 'A man sees in the world what he carries in his heart. — Goethe' \
-            /tmp/wallpaper.png
+            $lockpng
 fi
 
-# Pause music (mocp and mpd):
 playerctl pause
 
-# Lock it up!
-i3lock -e -f -c 000000 -i /tmp/wallpaper.png
-
-# If still locked after 20 seconds, turn off screen.
-#sleep 30 && pgrep i3lock && xset dpms force off
+i3lock -e -f -c 000000 -i $lockpng
